642a3d87c6c814aa4c3710ea281200803a2631e6
Alex Lang <alex.lang@gmail.com>
2020-02-22 18:33:52 -0800

    Store core exception

--- wrapped_exception.py
+++ wrapped_exception.py
@@ -6,6 +6,7 @@ class WrappedException(Exception):
     def __init__(self, exception):
         super(WrappedException, self).__init__(exception.message)
         self.exc_info = sys.exc_info()
+        self.core_exception = exception
 
     def print_core_stacktrace(self):
         traceback.print_tb(self.exc_info[2])
@@ -14,7 +15,7 @@ class WrappedException(Exception):
         return traceback.format_tb(self.exc_info[2])
 
     def get_core_exception(self):
-        return self.exc_info[1]
+        return self.core_exception
 
 try:
     raise Exception("HELLOW WORLD")
