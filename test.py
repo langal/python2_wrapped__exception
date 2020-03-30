742cf0f0f1734fb7ee0286e93106d03920bd27bc
Alex Lang <alex.lang@gmail.com>
2020-03-30 12:35:18 -0700

    Add tests

--- test.py
+++ test.py
@@ -1,28 +1,50 @@
+import sys
+import traceback
+
 from wrapped_exception import WrappedException
 
-"""
-Verify that we can catch and re-raise a Key Violation Exception
-as a "WrappedException".
-"""
-try:
-    try:
-        an_array = []
-        an_array[9]
-    except Exception as e:
-        wrap_exc = WrappedException(e)
-        wrap_exc.throw()
-except WrappedException:
-    pass
-else:
-    assert(False)
 
-try:
+def test_catch_raise():
+    """Verify that we can catch and re-raise a Key Violation Exception as a "WrappedException"."""
     try:
-        an_array = []
-        an_array[9]
-    except Exception:
-        WrappedException().throw()
-except WrappedException:
-    pass
-else:
-    assert(False)
+        try:
+            _my_bad_code()
+        except Exception as e:
+            wrap_exc = WrappedException(e)
+            wrap_exc.throw()
+    except WrappedException as e:
+        assert(isinstance(e.exception, IndexError))
+        trace_back = sys.exc_info()
+
+        # verify that the bad code ('an_array[9') is in the stack trace
+        # stacktraces are a tuple (file, line number, function name, line)
+        assert('an_array[9]' == traceback.extract_tb(trace_back[2])[-1][-1])
+
+        # also verify that the stored trace_back in the Wrapped Exception works
+        assert('an_array[9]' == traceback.extract_tb(e.trace_back)[-1][-1])
+    else:
+        raise Exception("Failed to catch WrappedException")
+
+    # test that wrapped exception captures sys.exc_info
+    try:
+        try:
+            _my_bad_code()
+        except Exception:
+            WrappedException().throw()
+    except WrappedException as e:
+        assert(isinstance(e.exception, IndexError))
+        trace_back = sys.exc_info()
+        assert('an_array[9]' == traceback.extract_tb(trace_back[2])[-1][-1])
+        assert('an_array[9]' == traceback.extract_tb(e.trace_back)[-1][-1])
+    else:
+        raise Exception("Failed to catch WrappedException")
+
+# this is a function that just throws an IndexError
+def _my_bad_code():
+    # force a builtin exception
+    an_array = []
+    an_array[9]
+
+
+if __name__ == "__main__":
+    test_catch_raise()
