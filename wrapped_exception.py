3fe95faa8cdc1707d621adc44aa26917b0b11663
Alex Lang <alex.lang@gmail.com>
2020-03-29 18:02:00 -0700

    Change some comments

--- wrapped_exception.py
+++ wrapped_exception.py
@@ -1,19 +1,19 @@
 import sys
 import traceback
 
-class WrappedException(Exception):
-    """
-    This Exception class basically allows some Exception to be re-raised
-    as another Exception type (eg. a business exception).
 
-    Business-named exception would merely subclass this.
-    """
+class WrappedException(Exception):
+    """This Exception class basically allows some Exception to be re-raised
+    as another Exception type (eg. a business exception).
+    Business-named exception would merely subclass this."""
     def __init__(self, exception=None):
         super(WrappedException, self).__init__()
         self.exc_info = sys.exc_info()
+        self.trace_back = self.exc_info[2]
         self.exception = exception if exception else self.exc_info[1]
 
     def throw(self):
         if self.exc_info[0] and self.exc_info[1] and self.exc_info[2]:
             raise type(self), self.exception, self.exc_info[2]
         raise self
+
